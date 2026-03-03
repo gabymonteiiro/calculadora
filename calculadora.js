c5ed307997c1c546b9ed59abcd4a32a1c8b2ab49
gabymonteiiro <gabrielybmonteiro2007@gmail.com>
2026-03-03 12:23:04 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,2 @@
+import { somar, subtrair, multiplicar, dividir } from "./funcaosoma.js";
+import readline from "readline";
---
+++ testeexport.js
@@ -0,0 +1,11 @@
+import { somar } from "./funcaosoma.js";
+console.log(somar(2,3))
+
+import { subtrair } from "./funcaosoma.js";
+console.log(subtrair(2,3))
+
+import { dividir } from "./funcaosoma.js";
+console.log(dividir(2,3))
+
+import { multiplicar } from "./funcaosoma.js";
+console.log(multiplicar(2,3))
